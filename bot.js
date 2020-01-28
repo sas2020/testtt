07fa409e62eb32caa2d591d5bd1ebb3297013b0d
sas2020 <60392056+sas2020@users.noreply.github.com>
2020-01-28 15:39:44 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("671354854265847868")
+setInterval(function() {
+channel.send(`انا بحب الحلو`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
